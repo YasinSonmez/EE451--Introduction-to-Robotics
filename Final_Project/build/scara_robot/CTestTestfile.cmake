7f25af49bf05974047ae2af80898f494efd8cf03
Yasin <sonmezyasin6006@gmail.com>
2021-02-07 21:39:13 +0300

    Final Project added

---
+++ Final_Project/build/scara_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yasin/EE451/Final_Project/src/scara_robot
+# Build directory: /home/yasin/EE451/Final_Project/build/scara_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
